73142b258600543475c39c5e5e70b6150a603e82
ugluguglug <yyjsjl7@gmail.com>
2022-05-07 00:17:44 +0800

    added create room and score upload for socket

--- sockets.js
+++ sockets.js
@@ -1,54 +1,94 @@
 const fs = require('fs');
 
 const onlineUsers = {};
-const users = JSON.parse(fs.readFileSync("./database/users.json"));
+const rooms = {};
 
-console.log(users);
+function randomRoomId(username) {
+    let roomId = Math.floor(Math.random() * (65535 - 0 + 1)) + 0;
+    // If roomId in use
+    if (roomId in rooms) {
+        return false;
+    } else {
+        rooms[roomId.toString()] = { owner: username };
+        console.log(`Room ${roomId} created for ${username}`)
+        return true;
+    }
+}
 
 exports = module.exports = function (io) {
     io.on("connection", (socket) => {
-        console.log("Receive connection");
         let user = socket.request.session.user;
-        console.log(user);
+        let ip = socket.handshake.address.split(":")[3];
+        let id = socket.id
+
+        // Store online users in memory, with their ip address and socket id
         if (user) {
-            onlineUsers[user.username] = { highscore: users[user.username].highscore };
+            onlineUsers[user.username] = { ip: ip, id: id };
+            console.log(`User ${user.username} from ${ip}`);
         }
 
         // Notify other users that this guy joined
         io.emit("add user", JSON.stringify(user));
 
-        socket.on("users", () => {
-            console.log(onlineUsers);
+        // On creat room, message: None, create room give them the number
+        socket.on("create room", () => {
+            // Keep trying to find an empty room
+            while (!randomRoomId(user.username));
         })
 
-        socket.on("disconnect", () => {
-            delete onlineUsers[user.username];
-            io.emit("remove user", JSON.stringify(user));
-        });
+        // On join room, Message: room number(int), join room and start game
+        socket.on("join room", (roomId) => {
+            // check if roomId exists
 
+            // if not exist, start game
+        })
+
+        // On dice roll, Message: dices(array), send to partner
+        socket.on("dice roll", (dices) => {
+            // find opponent
+
+            // emit to opponent
+        })
+
+        // On gameover, Message: score
+        socket.on("score", (score) => {
+            console.log(`Receive score of ${score}`)
+            const users = JSON.parse(fs.readFileSync("./database/users.json"));
+            if (users[user.username] && users[user.username].highscore < score) {
+                users[user.username].highscore = score;
+            }
+            fs.writeFileSync("./database/users.json", JSON.stringify(users, null, " "));
+        })
+
+        // On get users, broadcast online users
         socket.on("get users", () => {
             socket.emit("users", JSON.stringify(onlineUsers));
         });
 
-        socket.on("get messages", () => {
-            console.log("get messages")
+        socket.on("debug", () => {
+            console.log(onlineUsers);
+            console.log(Object.keys(onlineUsers).length);
+            console.log(rooms);
+            console.log(Object.keys(rooms).length);
         })
 
-        socket.on("post message", (content) => {
-            let msg = {
-                user: user,
-                datetime: new Date(),
-                content: content
-            };
-            const chatroom = JSON.parse(fs.readFileSync("./data/chatroom.json"));
-            chatroom.push(msg);
-            fs.writeFileSync("./data/chatroom.json", JSON.stringify(chatroom, null, " "));
-            io.emit("add message", JSON.stringify(msg));
-        })
+        socket.on("disconnect", () => {
+            // Remove from memory
+            delete onlineUsers[user.username];
+            // Remove from other user's list
+            io.emit("remove user", JSON.stringify(user));
+        });
 
-        socket.on("typing", () => {
-            io.emit("typing broadcast", user.name);
-        })
-
+        // socket.on("post message", (content) => {
+        //     let msg = {
+        //         user: user,
+        //         datetime: new Date(),
+        //         content: content
+        //     };
+        //     const chatroom = JSON.parse(fs.readFileSync("./data/chatroom.json"));
+        //     chatroom.push(msg);
+        //     fs.writeFileSync("./data/chatroom.json", JSON.stringify(chatroom, null, " "));
+        //     io.emit("add message", JSON.stringify(msg));
+        // })
     });
 }
